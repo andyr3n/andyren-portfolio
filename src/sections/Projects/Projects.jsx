90de4bafcbae5575141af059b03eba0f98f02273
Andy Ren <74114355+andyr3n@users.noreply.github.com>
2025-01-06 18:05:29 -0800

    added projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,21 @@
 import styles from './ProjectStyles.module.css';
-import viberr from '../../assets/viberr.png';
+import ddmap from '../../assets/ddmap.png';
+import brokenomore from '../../assets/brokenomore.png';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
         <h1 className="sectionTitle">Projects</h1>
         <div className={styles.projectContainer}>
+            <a href="https://github.com/andyr3n/Broke-No-More" target="_blank">
+                <img className="hover" src={brokenomore} alt="Broke-No-More logo" />
+                <h3>Broke-No-More</h3>
+                <p>Android app designed to help students manage their finances with ease!</p>
+            </a>
             <a href="https://github.com/andyr3n/DDMap" target="_blank">
-                <img className="hover" src={viberr} alt="DDMap logo" />
+                <img className="hover" src={ddmap} alt="DDMap logo" />
                 <h3>DDMap</h3>
-                <p>Helps drivers share locations with markers and ratings.</p>
+                <p>Helps delivery drivers share location markers and ratings.</p>
             </a>
         </div>
     </section>
