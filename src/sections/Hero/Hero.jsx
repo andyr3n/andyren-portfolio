d5c69a9724f03e6a90ed45bf50a5f097eb3c724b
Andy Ren <andyren2001@gmail.com>
2025-02-03 15:36:00 -0800

    changed pfp

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Andy.png';
+import heroImg from '../../assets/Andy1.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import githubLight from '../../assets/github-light.svg';
