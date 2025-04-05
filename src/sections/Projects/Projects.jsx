e39a7e29af4647a14967db4413302670499e5534
Andy Ren <andyren2001@gmail.com>
2025-04-04 21:12:33 -0700

    updated project list

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -7,15 +7,15 @@ function Projects() {
     <section id="projects" className={styles.container}>
         <h1 className="sectionTitle">Projects</h1>
         <div className={styles.projectContainer}>
-            <a href="https://github.com/andyr3n/Broke-No-More" target="_blank">
-                <img className="hover" src={brokenomore} alt="Broke-No-More logo" />
-                <h3>Broke-No-More</h3>
-                <p>Android app designed to help students manage their finances with ease!</p>
-            </a>
             <a href="https://github.com/andyr3n/Spots-Map" target="_blank">
                 <img className="hover" src={ddmap} alt="DDMap logo" />
                 <h3>Spots Map</h3>
                 <p>Interactive Location Sharing</p>
+            </a>
+            <a href="https://github.com/andyr3n/Broke-No-More" target="_blank">
+                <img className="hover" src={brokenomore} alt="Broke-No-More logo" />
+                <h3>Broke-No-More</h3>
+                <p>Android app designed to help students manage their finances with ease!</p>
             </a>
         </div>
     </section>
