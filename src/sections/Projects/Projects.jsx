441f6f9105c4d36ea2a4919ffbd8db13d2b3c869
Andy Ren <andyren2001@gmail.com>
2025-04-25 00:00:33 -0700

    updated project description

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -10,12 +10,12 @@ function Projects() {
             <a href="https://github.com/andyr3n/Spots-Map" target="_blank">
                 <img className="hover" src={ddmap} alt="DDMap logo" />
                 <h3>Spots Map</h3>
-                <p>Interactive Location Sharing</p>
+                <p>Spots Map is a full-stack MERN (MongoDB, Express.js, React, Node.js) application that allows users to mark locations on a map, leave reviews, and rate places.</p>
             </a>
             <a href="https://github.com/andyr3n/Broke-No-More" target="_blank">
                 <img className="hover" src={brokenomore} alt="Broke-No-More logo" />
                 <h3>Broke-No-More</h3>
-                <p>Android app designed to help students manage their finances with ease!</p>
+                <p>A Kotlin-based finance app for students to manage budgets, savings, and subscriptions. Features an AI chatbot with OpenAI API for personalized financial tips.</p>
             </a>
         </div>
     </section>
